4693b033804a8ecd6ea0058f49dc4cdeb7b7b4e0
Christopher Rosset <chris@rosset.tech>
2021-09-19 14:27:42 -0400

    MQTT improvements
    - added unique IDs to link entities to the device in HA
    - availability messages for the switch updated on startup and shutdown

--- mamaroo_mqtt.py
+++ mamaroo_mqtt.py
@@ -16,13 +16,12 @@ def base_mqtt_topic(prefix, mac, component="select"):
     return f"{prefix}/{component}/mamaroo/{mac.replace(':', '_')}"
 
 class MqttPoster():
-    _loop = None
     _mac = None
     _mqtt = None
     _status = None
+    _available = False
 
-    def __init__(self, loop, mqtt, mac, prefix):
-        self._loop = loop
+    def __init__(self, mqtt, mac, prefix):
         self._mac = mac
         self._mqtt = mqtt
         self._prefix = prefix
@@ -38,17 +37,23 @@ class MqttPoster():
 
         self._status = status
 
-        self._loop.create_task(self._mqtt.publish(
+        asyncio.create_task(self._mqtt.publish(
             f"{base_mqtt_topic(self._prefix, self._mac, 'switch')}/state",
-            payload=str(status["power"]).encode(), retain=True))
+            payload=str(status["power"]), retain=True))
 
-        self._loop.create_task(self._mqtt.publish(
+        asyncio.create_task(self._mqtt.publish(
             f"{base_mqtt_topic(self._prefix, self._mac)}-mode/state",
-            payload=MODES[status["mode"]].encode(), retain=True))
+            payload=MODES[status["mode"]], retain=True))
 
-        self._loop.create_task(self._mqtt.publish(
+        asyncio.create_task(self._mqtt.publish(
             f"{base_mqtt_topic(self._prefix, self._mac)}-speed/state",
-            payload=str(status["speed"]).encode(), retain=True))
+            payload=str(status["speed"]), retain=True))
+
+        if not self._available:
+            self._available = True
+            asyncio.create_task(self._mqtt.publish(
+                f"{base_mqtt_topic(self._prefix, self._mac, 'switch')}/availability",
+                payload="online", retain=True))
 
 def clamp(minimum, maximum, value):
     return sorted([minimum, maximum, value])[1]
@@ -69,17 +74,13 @@ def validate_command_message(data):
     return ("mode" in data and isinstance(data["mode"], int)
             and "speed" in data and isinstance(data["speed"], int))
 
-async def run(loop, mqtt, bt, args):
-    poster = MqttPoster(loop, mqtt, args.MAC, args.prefix)
-
-    await bt.start_notify(UUID, poster)
-
+async def run(mqtt, bt, args):
     async with mqtt.unfiltered_messages() as messages:
         await mqtt.subscribe(f"{args.prefix}/+/mamaroo/+/command")
         async for message in messages:
             try:
                 payload = message.payload.decode()
-                logging.info("Incoming MQTT topic={message.topic} message={payload}")
+                logging.info("Incoming MQTT topic={} message={}".format(message.topic, payload))
 
                 parts = message.topic.split('/')
                 component = parts[1]
@@ -107,6 +108,7 @@ async def run(loop, mqtt, bt, args):
                 logging.info(e)
 
 async def publish_autodiscovery_data(mqtt, args):
+
     mac = args.MAC
     base_topic = base_mqtt_topic(args.prefix, mac)
 
@@ -114,21 +116,27 @@ async def publish_autodiscovery_data(mqtt, args):
         "name": "mamaroo4 infant seat",
         "manufacturer": "4moms",
         "model": "mamaRoo4",
-        "connections": [['mac', args.MAC]]
+        "connections": [['mac', args.MAC]],
+        "identifiers": [args.MAC]
     }
 
     if args.serial:
-        device["identifiers"] = args.serial
+        device["identifiers"].append(args.serial)
+
+    def unique_id(name):
+        return f"mamaroo-{mac.replace(':', '-').lower()}-{name}"
 
     switch_topic = f"{base_mqtt_topic(args.prefix, mac, 'switch')}"
     await mqtt.publish(f"{switch_topic}/config", json.dumps({
         "name": f"Mamaroo {mac} Switch",
+        "availability_topic": f"{switch_topic}/availability",
         "command_topic": f"{switch_topic}/command",
         "state_topic": f"{switch_topic}/state",
         "payload_on": "1",
         "payload_off": "0",
         "device": device,
         "icon": "mdi:rocket-launch",
+        "unique_id": unique_id('switch')
     }), retain=True)
 
     mode_topic = f"{base_topic}-mode"
@@ -137,7 +145,8 @@ async def publish_autodiscovery_data(mqtt, args):
         "command_topic": f"{mode_topic}/command",
         "state_topic": f"{mode_topic}/state",
         "options": MODES[1:],
-        "device": device
+        "device": device,
+        "unique_id": unique_id('mode')
     }), retain=True)
 
     speed_topic = f"{base_topic}-speed"
@@ -148,19 +157,12 @@ async def publish_autodiscovery_data(mqtt, args):
         "options": ["0", "1", "2", "3", "4", "5"],
         "device": device,
         "icon": "mdi:speedometer",
+        "unique_id": unique_id('speed')
     }), retain=True)
 
     logging.info("Auto-discovery messages published.")
 
-async def start(loop, args):
-    async with asyncio_mqtt.Client(args.broker) as mqtt:
-        logging.info("MQTT connection to {} established.".format(args.broker))
-        async with bleak.BleakClient(args.MAC, timeout=5, loop=loop) as bt:
-                logging.info("Bluetooth connection to {} established.".format(args.MAC))
-                await publish_autodiscovery_data(mqtt, args)
-                await run(loop, mqtt, bt, args)
-
-def main():
+async def main():
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                         level=logging.INFO)
 
@@ -187,10 +189,30 @@ def main():
         # will prevent a new instance from establishing a new connection. This
         # is a quick way to clear this on startup.
         subprocess.call(['bluetoothctl','disconnect', args.MAC], timeout=10)
-        loop.run_until_complete(start(loop, args))
+
+        availability_topic = f"{base_mqtt_topic(args.prefix, args.MAC, 'switch')}/availability"
+        last_will = asyncio_mqtt.Will(availability_topic, payload="offline", retain=True)
+
+        async with asyncio_mqtt.Client(args.broker, will=last_will) as mqtt:
+            logging.info("MQTT connection to {} established.".format(args.broker))
+            async with bleak.BleakClient(args.MAC, timeout=5, loop=loop) as bt:
+                logging.info("Bluetooth connection to {} established.".format(args.MAC))
+
+                try:
+                    await publish_autodiscovery_data(mqtt, args)
+                    poster = MqttPoster(mqtt, args.MAC, args.prefix)
+                    await bt.start_notify(UUID, poster)
+                    mqtt_task = asyncio.create_task(run(mqtt, bt, args))
+                    await asyncio.Future()
+                finally:
+                    logging.info("Exception thrown, running cleanup...")
+                    await bt.stop_notify(UUID)
+                    mqtt_task.cancel()
+                    await mqtt.publish(availability_topic, payload="offline", retain=True)
+
     except bleak.exc.BleakDBusError as e:
-        logging.info(e)
+        logging.error(e)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
